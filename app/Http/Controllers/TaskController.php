085034c80c37942acb7c345241decd2d288b3935
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-12-14 17:48:05 +0900

    index.blade.phpを作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return "Hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+
+            //第一引数がテンプレート名で第二引数がテンプレートに渡すデータ、第二引数には配列を渡す、キーがテンプレートで参照する際の変数名"
+            'folders' => $folders,
+        ]);
     }
     //
 }
