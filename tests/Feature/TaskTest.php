f1c633d33ae04f59b55bcfd021a963854d0ddb87
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-12-22 17:13:56 +0900

    foldersテーブルの中にuser_idカラムを追加

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,29 +2,22 @@
 
 namespace Tests\Feature;
 
-use App\Http\Requests\CreateTask;
 use Carbon\Carbon;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TaskTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     
-    public function setUp() : void
+    public function setUp(): void
     {
         parent::setUp();
         //テストケース実行前にフォルダデータを作成する
         $this->seed('FoldersTableSeeder');
     }
 
-    public function doe_date_should_be_date()
+    public function due_date_should_be_date()
     {
         $response = $this->post('/folders/1/tasks/create', [
             'title' => 'Sample task',
@@ -32,22 +25,10 @@ class TaskTest extends TestCase
         ]);
 
         $response->assertSessionHasErrors([
-            'due_date' => '期限日には日付を入力してください。'
+            'due_date' => '期限日には日付を入力してください。',
         ]);
 
     }
-
-    // public function test_due_date_sholud_not_be_past()
-    // {
-    //     $response = $this->post('folders/1/tasks/create', [
-    //         'title' => 'Sample Task',
-    //         'due_date' => Carbon::yesterday()->format('Y/m/d'),
-    //     ]);
-
-    //     $response->assertSessionHasErrors([
-    //         'due_date' => '期限日には今日以降の日付を入力してください。',
-    //     ]);
-    // }
 
     public function due_date_should_not_be_past()
     {
@@ -72,14 +53,7 @@ class TaskTest extends TestCase
         ]);
 
         $response->assertSessionHasErrors([
-            'status' => '状態には　未着手、着手中、完了のいずれかを指定してください。'
+            'status' => '状態には 未着手、着手中、完了のいずれかを指定してください。',
         ]);
     }
-
-    // public function testExample()
-    // {
-    //     $response = $this->get('/');
-
-    //     $response->assertStatus(200);
-    // }
 }
